e7a7ea5b6aad13ca572a74046e4d93072b45087e
theubaidistan <ubaidistan395@gmail.com>
2025-12-25 00:47:28 +0500

    Add favicon images for the application
    - Added favicon-16x16.png for the 16x16 pixel size.
    - Added favicon-32x32.png for the 32x32 pixel size.

--- src/index.ts
+++ src/index.ts
@@ -81,8 +81,12 @@ app.get("/", (c) => {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Checkout</title>
+      <title>Stripe Checkout Backend</title>
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png">
+      <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png">
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
 
       <script src="https://js.stripe.com/v3/"></script>
       <style>
